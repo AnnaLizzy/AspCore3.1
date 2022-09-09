443b445ec885e1dadd087fc6b6e8c946b4d3fe03
AnnaLizzy <dinhmanh1995@gmail.com>
2022-09-09 13:02:02 +0700

    add

--- WebApp.Data/EF/AppDbContext.cs
+++ WebApp.Data/EF/AppDbContext.cs
@@ -19,6 +19,8 @@ namespace WebApp.Data.EF
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
             modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
             //base.OnModelCreating(modelBuilder);
         }
 
