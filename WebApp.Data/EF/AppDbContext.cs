00b63d805bd636c035b2b7a7962f670409d287c4
AnnaLizzy <thuthuha29@gmail.com>
2022-09-10 10:48:50 +0700

    test

--- WebApp.Data/EF/AppDbContext.cs
+++ WebApp.Data/EF/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,7 +9,7 @@ using WebApp.Data.Extensions;
 
 namespace WebApp.Data.EF
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions options) : base(options)
         {         
